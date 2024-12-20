48253a1d957b1b1b9d4d66c0d309bd0608af8b14
wnhyang <wnhyang@qq.com>
2024-12-20 16:34:49 +0800

    项目调整，加入es数据存储

---
+++ coolGuard-decision/src/main/java/cn/wnhyang/coolGuard/es/ElasticSearchService.java
@@ -0,0 +1,30 @@
+package cn.wnhyang.coolGuard.es;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+/**
+ * @author wnhyang
+ * @date 2024/12/19
+ **/
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ElasticSearchService {
+
+    private final RestHighLevelClient restHighLevelClient;
+
+    public void indexDocument(String indexName, String json) throws IOException {
+        IndexRequest request = new IndexRequest(indexName).source(json, XContentType.JSON);
+        IndexResponse response = restHighLevelClient.index(request, RequestOptions.DEFAULT);
+        log.info("Indexed document with ID: " + response.getId());
+    }
+}
--- coolGuard-decision/src/main/java/cn/wnhyang/coolGuard/kafka/consumer/EventESDataConsumer.java
+++ coolGuard-decision/src/main/java/cn/wnhyang/coolGuard/kafka/consumer/EventESDataConsumer.java
@@ -1,6 +1,8 @@
 package cn.wnhyang.coolGuard.kafka.consumer;
 
 import cn.wnhyang.coolGuard.constant.KafkaConstant;
+import cn.wnhyang.coolGuard.es.ElasticSearchService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -11,11 +13,20 @@ import org.springframework.stereotype.Service;
  **/
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class EventESDataConsumer {
+
+    private final ElasticSearchService elasticSearchService;
 
     @KafkaListener(topics = KafkaConstant.EVENT_ES_DATA, groupId = KafkaConstant.EVENT_ES_DATA_GROUP)
     public void consume(String message) {
         // TODO 存储到ES
         log.info("consume message: {}", message);
+        try {
+            elasticSearchService.indexDocument("event_20241219", message);
+        } catch (Exception e) {
+            log.error("存储到ES失败", e);
+        }
+
     }
 }
